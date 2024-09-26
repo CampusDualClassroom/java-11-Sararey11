83303173f97e7cbd7a136b950e63efce8e0e1c4f
Sara <sarareyvalino23@gmail.com>
2024-09-26 11:05:47 +0200

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -2,4 +2,62 @@ package com.campusdual.classroom;
 
 public class TVRemote {
 
+    int channel = 0;
+    int volume = 0;
+    boolean on;
+    String color;
+
+    public TVRemote(int channel, int volume, String color) {
+        this.channel = 0;
+        this.volume = 20;
+        this.color = color;
+    }
+
+    public TVRemote(String color) {
+        this.color = color;
+    }
+
+    public boolean turnOn(){
+        this.on = true;
+        return this.on;
+    }
+
+    public boolean turnOff(){
+        this.on = false;
+        return this.on;
+    }
+
+    public void channelUp(){
+        this.channel++;
+    }
+
+    public void channelDown(){
+        if(this.checkMinValue(this.channel)){
+            this.channel--;
+        }
+    }
+
+    public void volumeUp(){
+        this.volume++;
+    }
+
+    public void volumeDown(){
+        if(this.checkMinValue(this.volume)){
+            this.volume--;
+        }
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+
+    private boolean checkMinValue(int value){
+        if(value == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
 }
